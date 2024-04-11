ea4f62d22c2921be60f7ba128cf85e159e358de8
Maciej Grzybacz <128727060+MaciejGrzybacz@users.noreply.github.com>
2024-04-11 12:27:32 +0200

    Delete zaj1Fraction directory

--- zaj1Fraction/cmake-build-debug/tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/006ma/OneDrive/Pulpit/C++/lab1-fraction-main/lab1-fraction-main/zaj1Fraction/tests/lib/googletest
-# Build directory: C:/Users/006ma/OneDrive/Pulpit/C++/lab1-fraction-main/lab1-fraction-main/zaj1Fraction/cmake-build-debug/tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
